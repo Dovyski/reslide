27e220037f7fd7875c99a74d3a6a495b6b61d1ce
Fernando Bevilacqua <dovyski@gmail.com>
2017-08-08 16:44:28 +0200

    Basic sync is partially working

---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('DATA_DIR', dirname(__FILE__) . '/../data/');
+
+ ?>
--- api/index.php
+++ api/index.php
@@ -1,27 +1,64 @@
 <?php
 
-$aMethod = isset($_REQUEST['method']) ? $_REQUEST['method'] : '';
-$aId = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
-$aReturn = array('success' => true, 'method' => $aMethod, 'time' => time());
+require_once(dirname(__FILE__) . '/config.php');
+
+$aMethod    = isset($_REQUEST['method']) ? $_REQUEST['method'] : '';
+$aId        = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
+$aPresenter = isset($_REQUEST['presenter']) ? $_REQUEST['presenter'] : '';
+$aReturn    = array('success' => true, 'method' => $aMethod, 'time' => time());
 
 try {
-	if($aId == '') {
-		throw new Exception('Empty id');
-	}
-
 	switch ($aMethod) {
 		case 'create':
-			$aReturn['data'] = 'Test';
-			break;
-		case 'read':
+			$aPresenterId = md5(rand() . 'blah' . time()); // TODO: improve this
+			$aPresenterHash = md5($aPresenterId);
+			$aViewerId = md5(rand() . $aPresenterHash);
+			$aData = array(
+				'viewer_id' => $aViewerId
+			);
+
+			file_put_contents(DATA_DIR . DIRECTORY_SEPARATOR . $aPresenterHash, serialize($aData));
+
 			$aReturn['data'] = array(
-				'slide' => 2,
-				'write_time' => time()
+				'presenter_id' => $aPresenterId
 			);
 			break;
-		case 'write':
-			$aReturn['slide'] = 'Test';
+
+		case 'read':
+			if($aId == '') {
+				throw new Exception('Empty id');
+			}
+			$aHash = md5($aId);
+			$aSyncData = unserialize(@file_get_contents(DATA_DIR . DIRECTORY_SEPARATOR . $aHash));
+
+			if($aSyncData === false) {
+				throw new Exception('Something wrong with the unserialize.');
+			}
+
+			$aReturn['data'] = $aSyncData;
 			break;
+
+		case 'write':
+			if($aPresenter == '') {
+				throw new Exception('Missing presenter identification.');
+			}
+
+			$aPresenterHash = md5($aPresenter);
+			$aPresenterData = unserialize(@file_get_contents(DATA_DIR . DIRECTORY_SEPARATOR . $aPresenterHash));
+
+			if($aPresenterData === false) {
+				throw new Exception('Unable to load presenter data.');
+			}
+
+			$aViewerPackage = array(
+				'slide' => isset($_REQUEST['slide']) ? $_REQUEST['slide'] : '1',
+				'write_time' => time()
+			);
+
+			$aViewerHash = md5($aPresenterData['viewer_id']);
+			file_put_contents(DATA_DIR . DIRECTORY_SEPARATOR . $aViewerHash, serialize($aViewerPackage));
+			break;
+
 		default:
 			$aReturn = array('failure' => true, 'message' => 'Unknow method');
 			break;
